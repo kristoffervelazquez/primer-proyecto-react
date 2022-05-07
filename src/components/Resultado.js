e5271331f913c322fc435e5587af7dbee9acad18
kristoffervelazquez <kristoffervelazquez49@live.com>
2022-05-06 22:18:48 -0700

    Se modifica el componente resultado

--- src/components/Resultado.js
+++ src/components/Resultado.js
@@ -6,10 +6,10 @@ const Resultado = (props) => {
     return ( 
         <div className="u-full-width resultado">
             <h2>Resumen</h2>
-            <p>La cantidad solicitada es: ${cantidad}</p>
-            <p>A pagar en un plazo de {plazo} meses</p>
-            <p>Su pago mensual es de: ${(total/plazo).toFixed(2)} </p>
-            <p>Total a pagar: ${(total).toFixed(2)}</p>
+            <p>La cantidad solicitada es: <strong>${cantidad}</strong></p>
+            <p>A pagar en un plazo de <strong>{plazo}</strong> meses</p>
+            <p>Su pago mensual es de: <strong>${(total/plazo).toFixed(2)}</strong></p>
+            <p>Total a pagar: <strong>${(total).toFixed(2)}</strong></p>
         </div>
      );
 }
